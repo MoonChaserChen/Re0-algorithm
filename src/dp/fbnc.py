7225e92292ddaa1a530090df1b63668a258d5b03
akira <chin.kou.akira@gmail.com>
2019-08-07 10:20:48 +0800

    【新增】动态规划-斐波那契数列-自底向上省空间

--- src/dp/fbnc.py
+++ src/dp/fbnc.py
@@ -3,11 +3,7 @@ import timeit
 
 # 递归
 def fbnc(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fbnc(n - 1) + fbnc(n - 2)
+    return n if n < 2 else fbnc(n - 1) + fbnc(n - 2)
 
 
 # 自顶向下
@@ -17,7 +13,7 @@ def fbnc_dp0(n):
     def fbnc_dp_arr(i, arr):
         if arr[i]:
             return arr[i]
-        if i == 0 or i == 1:
+        if i < 2:
             return i
         arr[i] = fbnc_dp_arr(i - 1, arr) + fbnc_dp_arr(i - 2, arr)
         return arr[i]
@@ -34,6 +30,17 @@ def fbnc_dp1(n):
     return dp[n]
 
 
-print("fbnc time take:", timeit.timeit("fbnc(15)", setup="from __main__ import fbnc", number=1000))
-print("fbnc_dp0 time take:", timeit.timeit("fbnc_dp0(15)", setup="from __main__ import fbnc_dp0", number=1000))
-print("fbnc_dp1 time take:", timeit.timeit("fbnc_dp1(15)", setup="from __main__ import fbnc_dp1", number=1000))
+# 自底向上省空间
+def fbnc_dp2(n):
+    if n < 2:
+        return n
+    tmp1, tmp2 = 0, 1
+    for i in range(2, n + 1):
+        tmp1, tmp2 = tmp2, tmp1 + tmp2
+    return tmp2
+
+
+print("fbnc get result:", fbnc(15), ",time take:", timeit.timeit("fbnc(15)", setup="from __main__ import fbnc", number=1000))
+print("fbnc_dp0 result:", fbnc_dp0(15), ",time take:", timeit.timeit("fbnc_dp0(15)", setup="from __main__ import fbnc_dp0", number=1000))
+print("fbnc_dp1 result:", fbnc_dp1(15), ",time take:", timeit.timeit("fbnc_dp1(15)", setup="from __main__ import fbnc_dp1", number=1000))
+print("fbnc_dp2 result:", fbnc_dp2(15), ",time take:", timeit.timeit("fbnc_dp2(15)", setup="from __main__ import fbnc_dp2", number=1000))
