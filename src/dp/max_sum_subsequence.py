f335fd79eb55fc4456f0031a609de2b00be75adb
akira <chin.kou.akira@gmail.com>
2019-08-16 21:17:59 +0800

    【新增】动态规划：最大连续子序列之和：动态规划

--- src/dp/max_sum_subsequence.py
+++ src/dp/max_sum_subsequence.py
@@ -25,8 +25,62 @@ class MaxSumSubSequence:
         le = len(self.arr)
         return max(ite_step(i) for i in range(le))
 
+    # 自顶向下动态规则
+    def dp(self):
+        le = len(self.arr)
+        dp = [None] * le
 
-data = [16, 29, 0, -13, -15, -21, 3, 22, 2, -26]
+        # 以index为ix结尾的连续子序列最大和
+        def ite_step(ix):
+            if ix == 0:
+                return self.arr[0]
+            if dp[ix]:
+                return dp[ix]
+            dp[ix] = self.arr[ix] if ite_step(ix - 1) <= 0 else self.arr[ix] + ite_step(ix - 1)
+            return dp[ix]
+
+        return max(ite_step(i) for i in range(le))
+
+    # 自底向上动态规则
+    def dp1(self):
+        le = len(self.arr)
+        dp = [None] * le
+        dp[0] = self.arr[0]
+        # dp[i]表示以index为i结尾的连续子序列最大和
+        for i in range(1, le):
+            dp[i] = dp[i - 1] + self.arr[i] if dp[i - 1] > 0 else self.arr[i]
+        return max(dp)
+
+    # 自底向上省空间
+    def dp2(self):
+        le = len(self.arr)
+        # max_sum 表示连续子序列最大和
+        # max_step表示以index为i结尾的连续子序列最大和
+        # 将上面dp1()方法的的dp[]用max_sum和max_step来表示
+        max_sum = max_step = self.arr[0]
+        for i in range(1, le):
+            max_step = self.arr[i] if max_step <= 0 else max_step + self.arr[i]
+            max_sum = max(max_sum, max_step)
+        return max_sum
+
+    # 自底向上省空间再优化
+    def dp3(self):
+        le = len(self.arr)
+        max_sum = max_step = self.arr[0]
+        for i in range(1, le):
+            # 最大连续子序列的结尾是不可能小于0的
+            if self.arr[i] <= 0:
+                continue
+            max_step = self.arr[i] if max_step <= 0 else max_step + self.arr[i]
+            max_sum = max(max_sum, max_step)
+        return max_sum
+
+
+data = [-14, -19, 8, 7, 7, 7, 3, 11, 8, -9, -6, -1]
 mss = MaxSumSubSequence(data)
-print("Bruce result:", mss.bruce(), ", take time:", timeit.timeit("MaxSumSubSequence([16, 29, 0, -13, -15, -21, 3, 22, 2, -26]).bruce()", setup="from __main__ import MaxSumSubSequence", number=10000))
-print("Bruce result:", mss.ite(), ", take time:", timeit.timeit("MaxSumSubSequence([16, 29, 0, -13, -15, -21, 3, 22, 2, -26]).ite()", setup="from __main__ import MaxSumSubSequence", number=10000))
+print("bruce result:", mss.bruce(), ", take time:", timeit.timeit("MaxSumSubSequence([-14, -19, 8, 7, 7, 7, 3, 11, 8, -9, -6, -1]).bruce()", setup="from __main__ import MaxSumSubSequence", number=10000))
+print("ite result:", mss.ite(), ", take time:", timeit.timeit("MaxSumSubSequence([-14, -19, 8, 7, 7, 7, 3, 11, 8, -9, -6, -1]).ite()", setup="from __main__ import MaxSumSubSequence", number=10000))
+print("dp from top result:", mss.dp(), ", take time:", timeit.timeit("MaxSumSubSequence([-14, -19, 8, 7, 7, 7, 3, 11, 8, -9, -6, -1]).dp()", setup="from __main__ import MaxSumSubSequence", number=10000))
+print("dp from bottom result:", mss.dp1(), ", take time:", timeit.timeit("MaxSumSubSequence([-14, -19, 8, 7, 7, 7, 3, 11, 8, -9, -6, -1]).dp1()", setup="from __main__ import MaxSumSubSequence", number=10000))
+print("dp from bottom save space result:", mss.dp2(), ", take time:", timeit.timeit("MaxSumSubSequence([-14, -19, 8, 7, 7, 7, 3, 11, 8, -9, -6, -1]).dp2()", setup="from __main__ import MaxSumSubSequence", number=10000))
+print("dp from bottom save space optimization result:", mss.dp3(), ", take time:", timeit.timeit("MaxSumSubSequence([-14, -19, 8, 7, 7, 7, 3, 11, 8, -9, -6, -1]).dp3()", setup="from __main__ import MaxSumSubSequence", number=10000))
