5acf2d153da8285f05d5774304488bde2f05b684
akira <chin.kou.akira@gmail.com>
2020-01-30 18:09:19 +0800

    add article about: 回文数字.md

--- src/test.py
+++ src/test.py
@@ -1,3 +1,14 @@
-print(3 ** 4) # 81
-print(-2 ** 4) # -16
-print(2 ** 4 -1) # 15
+def is_palindrome(x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0 or x % 10 == 0 and x != 0:
+        return False
+    r_x = 0
+    while x > r_x:
+        r_x = r_x * 10 + x % 10
+        x //= 10
+    return x == r_x or x == r_x // 10
+
+print(is_palindrome(123))
