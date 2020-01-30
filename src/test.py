7ec5c5ef39bda31536c144719be8b410b93e01f6
akira <chin.kou.akira@gmail.com>
2020-01-30 17:29:21 +0800

    change aritcle about grammar

--- src/test.py
+++ src/test.py
@@ -1,20 +1,3 @@
-def reverse(x):
-    """
-    :type x: int
-    :rtype: int
-    """
-    flag = 1 if x > 0 else -1
-    re = 0
-    abs_x = abs(x)
-    while abs_x > 0:
-        quo, rem = divmod(abs_x, 10)
-        re = re * 10 + rem
-        abs_x = quo
-    result = flag * re
-    return result if -2 ** 31 < result < 2 ** 31 - 1 else 0
-
-
-print((1 << 31) - 1)
-print(reverse(1534236469))
-# 1534236469
-# 2147483647
+print(3 ** 4) # 81
+print(-2 ** 4) # -16
+print(2 ** 4 -1) # 15
