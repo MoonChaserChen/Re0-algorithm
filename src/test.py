2b01f8b6e0f082a411e2ea8c431dfcc8aff7a4fe
akira <chin.kou.akira@gmail.com>
2020-02-01 21:23:32 +0800

    change details about grammar

--- src/test.py
+++ src/test.py
@@ -1,19 +1,3 @@
-def int2roman(n):
-    """
-    :type s: int
-    :rtype: str
-    """
-    d_r_m = {"I":1, "V":5, "X": 10, "L":50, "C":100, "D": 500, "M":1000}
-    result = ''
-    thousand, thousand_left = divmod(n, 1000)
-    for i in range(thousand):
-        result += 'M'
-    hundred, hundred_left = divmod(thousand_left, 100)
-    if hundred > 5:
-        pass
-
-print(int2roman(3))
-print(int2roman(4))
-print(int2roman(9))
-print(int2roman(58))
-print(int2roman(1994))
+print(list(zip([1, 2, 3], [4, 5]))) # [(1, 4), (2, 5)]
+L = ['a', 'b', 'c', 'd', 'e', 'f']
+print(list(zip(L[:-1], L[1:]))) # [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e'), ('e', 'f')]
