48efcca9d949e7b6f7356025840deeddb8487aa7
akira <chin.kou.akira@gmail.com>
2020-02-03 17:30:15 +0800

    add article: 合并两个有序链表.md

--- src/test.py
+++ src/test.py
@@ -1,18 +1,7 @@
-def is_valid(s):
-    """
-    :type s: str
-    :rtype: bool
-    """
-    if s is None: return False
-    if s == '': return True
-    dic = {"(": ")", "{": "}", "[": "]"}
-    stack = []
-    for x in s:
-        if x in dic:
-            stack.append(x)
-        else:
-            if stack and dic[stack[-1]] == x:
-                stack.pop()
-            else:
-                return False
-    return len(stack) == 0
+a = 10
+b = 5
+c = 4
+a, b, c = b, c, a
+print(a)
+print(b)
+print(c)
