88ba980baf8f09517244f17e985d5b1e62288165
akira <chin.kou.akira@gmail.com>
2020-02-04 12:25:18 +0800

    add article: 移除元素.md

--- src/test.py
+++ src/test.py
@@ -1,7 +1,18 @@
-a = 10
-b = 5
-c = 4
-a, b, c = b, c, a
-print(a)
-print(b)
-print(c)
+def remove_element(nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+    i, n = 0, len(nums)
+    while i < n:
+        if nums[i] == val:
+            nums[i] = nums[n - 1]
+            n -= 1
+        else:
+            i += 1
+    return i
+
+nums0 = [3, 3, 1, 2, 3, 4]
+remove_element(nums0, 3)
+print(nums0)
