9287b5ac058cac8bb2cb993ec8740c05dce35b36
akira <chin.kou.akira@gmail.com>
2020-02-04 21:46:24 +0800

    add article: 3.3-Sunday算法.md

--- src/test.py
+++ src/test.py
@@ -1,4 +1,26 @@
-print(list(range(10))) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(list(range(1, 10))) # [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(list(range(1, 10, 2))) # [1, 3, 5, 7, 9]
-print(list(range(9, -1, -2))) # [9, 7, 5, 3, 1]
+def cal_shift_mat(p):
+    dic = {}
+    len_p = len(p)
+    for i in range(len_p - 1, -1, -1):
+        if p[i] not in dic:
+            dic[p[i]] = len_p - i
+    return dic
+
+def find(s, p):
+    if p == "": return 0
+    len_s, len_p = len(s), len(p)
+    dic = cal_shift_mat(p)
+    idx = 0
+    while idx + len_p <= len_s:
+        if s[idx: idx + len_p] == p:
+            return idx
+        elif idx + len_p == len_s:
+            return -1
+        else:
+            idx += dic.get(s[idx + len_p], len_p + 1)
+    return -1
+
+print(find("abc", "c"))
+
+
+
