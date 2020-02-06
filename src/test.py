f94b8b2546ba557539ff3502293f1ba2eb890af9
akira <chin.kou.akira@gmail.com>
2020-02-06 11:51:55 +0800

    change details about two_sum

--- src/test.py
+++ src/test.py
@@ -1,3 +1,21 @@
-a1 = [1, 2, 3]
-a2 = [1, 2, 3]
-print(a1 == a2) # True
+def three_sum(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+
+def two_sum(nums, target):
+    x_dic = {}
+    re_dic = {}
+    re = []
+    for x in nums:
+        rem = target - x
+        if rem in x_dic:
+            re_dic[rem] = x
+        else:
+            x_dic[x] = rem
+    for k in re_dic:
+        re.append([k, re_dic[k]])
+    return re
+
+print(two_sum([1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 7, 1], 8)) # [[4, 4], [3, 5], [2, 6], [1, 7]]
