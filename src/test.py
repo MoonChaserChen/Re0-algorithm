d3aebc45c3b62a0cfed4799fe4b04ae9dd20c95b
akira <chin.kou.akira@gmail.com>
2020-02-07 21:57:31 +0800

    add article: x 的平方根.md

--- src/test.py
+++ src/test.py
@@ -1,16 +1,17 @@
-def add_binary(s1, s2):
-    i, j = len(s1) - 1, len(s2) - 1
-    plus = False
-    s = ''
-    while i >= 0 or j >= 0:
-        b1 = i >= 0 and s1[i] == '1'
-        b2 = j >= 0 and s2[j] == '1'
-        bv = b1 ^ b2 ^ plus
-        plus = b1 and b2 or b2 and plus or plus and b1
-        i, j = i - 1, j - 1
-        s += '1' if bv else '0'
-    if plus: s += '1'
-    return s[::-1]
-
-print(add_binary("1010", "1011"))
-print(add_binary("1010", "11"))
+def my_sqrt(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    if x <= 1: return x
+    lo, hi = 0, x
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        v = x // mid
+        if v == mid:
+            return mid
+        elif  v > mid:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return lo - 1
