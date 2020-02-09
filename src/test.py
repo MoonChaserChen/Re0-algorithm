46ebf169b787cf98edf1391b7e290f376cbad448
akira <chin.kou.akira@gmail.com>
2020-02-09 12:26:04 +0800

    add article: 二叉树的最大深度.md

--- src/test.py
+++ src/test.py
@@ -1,21 +1,9 @@
-from collections import deque
-
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
-def is_symmetric( root: TreeNode) -> bool:
-    if not root: return True
-    dq = deque([root.left, root.right])
-    while dq:
-        le, ri = dq.popleft(), dq.pop()
-        if not le and not ri: continue
-        elif not le or not ri: return False
-        elif le.val != ri.val: return False
-        dq.appendleft(le.right)
-        dq.appendleft(le.left)
-        dq.append(ri.left)
-        dq.append(ri.right)
-    return True
+def max_depth(root: TreeNode) -> int:
+    if root is None: return 0
+    return max(max_depth(root.left), max_depth(root.right)) + 1
