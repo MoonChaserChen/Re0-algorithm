61f684c33356ad859d9733c1b50ae3d0b9feccf9
akira <chin.kou.akira@gmail.com>
2020-02-12 18:04:34 +0800

    add article: 杨辉三角.md

--- src/test.py
+++ src/test.py
@@ -1,16 +1,11 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-def has_path_sum(root: TreeNode, target: int) -> bool:
-    if not root: return False
-    stack = [(root, root.val)]
-    while stack:
-        node, s = stack.pop()
-        if not node.left and not node.right and s == target:
-            return True
-        if node.right: stack.append((node.right, s + node.right.val))
-        if node.left: stack.append((node.left, s + node.left.val))
-    return False
+def generate(num_rows: int) -> list:
+    re = []
+    for i in range(num_rows):
+        level_re = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                level_re.append(1)
+            else:
+                level_re.append(re[i - 1][j - 1] + re[i - 1][j])
+        re.append(level_re)
+    return re
