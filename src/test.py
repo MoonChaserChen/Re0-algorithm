3d93a2aef7f7b344c5ffe82ce9a0a458a85bba18
akira <chin.kou.akira@gmail.com>
2020-02-14 20:10:52 +0800

    add article: 买卖股票的最佳时机.md

--- src/test.py
+++ src/test.py
@@ -1,28 +1,9 @@
-arr0 = [
-    [1],
-    [1,1],
-    [1,2,1],
-    [1,3,3,1],
-    [1,4,6,4,1]
-]
-
-def get_row(row_index: int) -> list:
-    arr = [1] * (row_index + 1)
-    if row_index < 2: return arr
-    for i in range(2, row_index + 1):
-        for j in range(i - 1, 0, -1):
-            arr[j] += arr[j - 1]
-    return arr
-
-print(get_row(0))
-print(get_row(1))
-print(get_row(2))
-print(get_row(3))
-print(get_row(4))
-print(get_row(5))
-print(get_row(6))
-print(get_row(7))
-print(get_row(8))
-print(get_row(9))
-print(get_row(10))
-
+def max_profit(prices: list) -> int:
+    min_val = None
+    profit = 0
+    for x in prices:
+        if min_val is None or x < min_val:
+            min_val = x
+        else:
+            profit = max(profit, x - min_val)
+    return profit
