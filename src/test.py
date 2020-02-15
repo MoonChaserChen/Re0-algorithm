95f3056564c6ebd9d3952a4d93b0ce7917f9c9a8
akira <chin.kou.akira@gmail.com>
2020-02-15 21:16:14 +0800

    add articles

--- src/test.py
+++ src/test.py
@@ -1,9 +1,16 @@
-def find_duplicate(nums: list) -> int:
-    slow, fast = 0, 0
-    slow, fast = nums[slow], nums[nums[fast]]
-    while slow != fast:
-        slow, fast = nums[slow], nums[nums[fast]]
-    n1, n2 = 0, slow
-    while n1 != n2:
-        n1, n2 = nums[n1], nums[n2]
-    return n1
+def single_number(nums: [int]) -> list:
+    # difference between two numbers (x and y) which were seen only once
+    bitmask = 0
+    for num in nums:
+        bitmask ^= num
+
+    # rightmost 1-bit diff between x and y
+    diff = bitmask & (-bitmask)
+
+    x = 0
+    for num in nums:
+        # bitmask which will contain only x
+        if num & diff:
+            x ^= num
+
+    return [x, bitmask^x]
