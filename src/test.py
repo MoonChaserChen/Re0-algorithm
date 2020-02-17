e7d052b11cc46de22a12d80e3ba1723e38e8bc80
akira <chin.kou.akira@gmail.com>
2020-02-17 15:30:31 +0800

    change details about grammar

--- src/test.py
+++ src/test.py
@@ -1,16 +1,6 @@
-def single_number(nums: [int]) -> [int]:
-    # difference between two numbers (x and y) which were seen only once
-    bitmask = 0
-    for num in nums:
-        bitmask ^= num
-
-    # rightmost 1-bit diff between x and y
-    diff = bitmask & (-bitmask)
-
-    x = 0
-    for num in nums:
-        # bitmask which will contain only x
-        if num & diff:
-            x ^= num
-
-    return [x, bitmask^x]
+a, b, c = 1, 1, 2
+print(a == b != c) # True
+print(a != c != b) # True
+print(c != b != a) # False
+print(c != b == a) # True
+print(-2 < a <= 1) # True
