afe3f0154494b263ed801c2d1578f5851a6024cb
akira <chin.kou.akira@gmail.com>
2020-02-18 22:07:59 +0800

    add article: 相交链表.md

--- src/test.py
+++ src/test.py
@@ -1,25 +1,11 @@
-class MinStack:
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.min_stack = []
-        self.val_stack = []
-
-
-    def push(self, x: int) -> None:
-        if not self.min_stack or x <= self.min_stack[-1]: self.min_stack.append(x)
-        self.val_stack.append(x)
-
-    def pop(self) -> int:
-        v = self.val_stack.pop()
-        if self.min_stack and v == self.min_stack[-1]:
-            self.min_stack.pop()
-        return v
-
-    def top(self) -> int:
-        if self.val_stack: return self.val_stack[-1]
-
-    def get_min(self) -> int:
-        if self.min_stack: return self.min_stack[-1]
+def get_intersection_node(head_a: ListNode, head_b: ListNode) -> ListNode:
+    ha, hb = head_a, head_b
+    while ha != hb:
+        ha = ha.next if ha else head_b
+        hb = hb.next if hb else head_a
+    return ha
