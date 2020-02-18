2d7ec044f0a95d2151bacc758ffe0bf1c0640496
akira <chin.kou.akira@gmail.com>
2020-02-18 20:38:29 +0800

    add article: 最小栈.md

--- src/test.py
+++ src/test.py
@@ -1,13 +1,25 @@
-def is_happy(n: int) -> bool:
-    slow, fast = hn(n), hn(hn(n))
-    while slow != fast:
-        slow, fast = hn(slow), hn(hn(fast))
-    return slow == 1
+class MinStack:
 
-def hn(n: int) -> int:
-    s = 0
-    while n:
-        r = n % 10
-        s += r * r
-        n //= 10
-    return s
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.min_stack = []
+        self.val_stack = []
+
+
+    def push(self, x: int) -> None:
+        if not self.min_stack or x <= self.min_stack[-1]: self.min_stack.append(x)
+        self.val_stack.append(x)
+
+    def pop(self) -> int:
+        v = self.val_stack.pop()
+        if self.min_stack and v == self.min_stack[-1]:
+            self.min_stack.pop()
+        return v
+
+    def top(self) -> int:
+        if self.val_stack: return self.val_stack[-1]
+
+    def get_min(self) -> int:
+        if self.min_stack: return self.min_stack[-1]
