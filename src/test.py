37b4b9655b52a2bd61299a19e037fb9f8c0950f2
akira <chin.kou.akira@gmail.com>
2020-02-22 23:13:36 +0800

    change details

--- src/test.py
+++
@@ -1,9 +0,0 @@
-def majority_element(nums: [int]) -> int:
-    s, majority = 0, None
-    for x in nums:
-        if s == 0: majority = x
-        if x != majority: s-= 1
-        else: s += 1
-    return majority
-
-print(majority_element([1, 2, 3, 2, 2, 1]))
