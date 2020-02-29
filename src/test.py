cff6ab95b22ad1a1e499d5c0d933cb9534bf6201
akira <chin.kou.akira@gmail.com>
2020-02-29 19:40:58 +0800

    change details

--- src/test.py
+++ src/test.py
@@ -1,22 +1,8 @@
-def rotate(nums: [int], k: int) -> None:
-    """
-    Do not return anything, modify nums in-place instead.
-    """
-    le = len(nums)
-    start, count = 0, 0
-    while start != le and count != le:
-        curr = start
-        tmp = nums[curr]
-        while True:
-            ne = (curr + k) % le
-            tmp, nums[ne] = nums[ne], tmp
-            curr = ne
-            count += 1
-            if curr == start: break
-        start += 1
-
-
 num = [1,2,3,4,5,6,7]
-k = 3
-rotate(num, k)
+
+
+def t(nums: [int]) -> None:
+    nums.reverse()
+
+t(num)
 print(num)
