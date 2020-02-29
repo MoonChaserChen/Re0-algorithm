7d00346cdb73b2f47f423e2ce71b10ce544682a3
akira <chin.kou.akira@gmail.com>
2020-02-29 19:28:41 +0800

    add article: 旋转数组.md

--- src/test.py
+++ src/test.py
@@ -1,6 +1,22 @@
-def trailing_zeroes(n: int) -> int:
-    c = 0
-    while n > 0:
-        n //= 5
-        c += n
-    return c
+def rotate(nums: [int], k: int) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    le = len(nums)
+    start, count = 0, 0
+    while start != le and count != le:
+        curr = start
+        tmp = nums[curr]
+        while True:
+            ne = (curr + k) % le
+            tmp, nums[ne] = nums[ne], tmp
+            curr = ne
+            count += 1
+            if curr == start: break
+        start += 1
+
+
+num = [1,2,3,4,5,6,7]
+k = 3
+rotate(num, k)
+print(num)
