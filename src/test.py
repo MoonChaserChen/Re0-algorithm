ef3cc71cfd6f44750197585201ea55f431b787e8
akira <chin.kou.akira@gmail.com>
2020-03-02 17:26:02 +0800

    add article: 反转链表.md

--- src/test.py
+++ src/test.py
@@ -1,25 +1,11 @@
-from collections import deque
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-
-class MyQueue:
-    def __init__(self):
-        self.stack1 = deque([])
-        self.stack2 = deque([])
-        self.first = None # stack1 栈底元素
-
-    def push(self, x: int) -> None:
-        if not self.stack1: self.first = x
-        self.stack1.append(x)
-
-    def pop(self) -> int:
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        return self.stack2.pop()
-
-    def peek(self) -> int:
-        if self.stack2: return self.stack2[-1]
-        return self.first
-
-    def empty(self) -> bool:
-        return len(self.stack1) == 0 and len(self.stack2) == 0
+def reverse_list(head: ListNode) -> ListNode:
+    if not head or not head.next: return head
+    p = reverse_list(head.next)
+    head.next.next = head
+    head.next = None
+    return p
