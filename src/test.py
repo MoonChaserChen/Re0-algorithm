b5347d99ace133dada3b82cd7fc35640539b9aa0
akira <chin.kou.akira@gmail.com>
2020-03-03 19:17:55 +0800

    add article: 汉明重量.md

--- src/test.py
+++ src/test.py
@@ -1,8 +1,6 @@
-def reverse_bits(n: int) -> int:
-    res = 0
-    c = 31
-    while n != 0:
-        res |= (n & 1) << c
-        c -= 1
+def hamming_weight(n: int) -> int:
+    re = 0
+    while n > 0:
+        re += n & 1
         n >>= 1
-    return res
+    return re
