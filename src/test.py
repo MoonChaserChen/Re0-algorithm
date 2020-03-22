23e8f13f9e0172a8d11883bb71e63450be7ebc73
akira <moon_chaser@163.com>
2020-03-22 19:23:03 +0800

    add article: 2.9-打家劫舍II.md

--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
 def rob(nums: [int]) -> int:
-    t1, t2 = 0, 0
-    for i in range(len(nums)):
-        t1, t2 = t2, max(t1 + nums[i], t2)
-    return t2
+    def my_rob(nums: [int]) -> int:
+        t1, t2 = 0, 0
+        for num in nums:
+            t1, t2 = t2, max(t1 + num, t2)
+        return t2
+    return max(my_rob(nums[1:]), my_rob(nums[:-1])) if len(nums) != 1 else nums[0]
