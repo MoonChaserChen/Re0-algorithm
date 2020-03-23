f18f99f6a76aa181eccb7270cd97565774183f95
akira <moon_chaser@163.com>
2020-03-23 11:09:26 +0800

    add article: 2.11-最大连续子序列乘积.md

--- src/test.py
+++ src/test.py
@@ -1,15 +1,12 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-def rob(root: TreeNode) -> int:
-    def rob_internal(node: TreeNode):
-        if not node: return 0, 0
-        re_left, re_right = rob_internal(node.left), rob_internal(node.right)
-        re0 = max(re_left) + max(re_right)
-        re1 = node.val + re_left[0] + re_right[0]
-        return re0, re1
-    return max(rob_internal(root))
+def max_product(nums: [int]) -> int:
+    max_re = max_step = min_step = None
+    for num in nums:
+        if max_re is None:
+            max_re = max_step = min_step = num
+        else:
+            candidate = [max_step * num, min_step * num, num]
+            max_step = max(candidate)
+            min_step = min(candidate)
+            if max_step > max_re:
+                max_re = max_step
+    return max_re
