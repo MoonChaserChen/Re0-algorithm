1d92d5e1c92fb941bcfbe4776003c5ceb4ea2623
akira <chin.kou.akira@gmail.com>
2020-03-29 19:28:24 +0800

    change details about grammar

--- src/test.py
+++ src/test.py
@@ -1,14 +1,12 @@
-string = 'hello'
-print(string.isalnum())     # 所有字符都是数字或者字母
-print(string.isalpha())     # 所有字符都是字母
-print(string.isdigit())     # 所有字符都是数字
-print(string.islower())     # 所有字符都是小写
-print(string.isupper())     # 所有字符都是大写
-print(string.istitle())     # 所有单词都是首字母大写，像标题
-print(string.isspace())     # 所有字符都是空白字符、\t、\n、\r
-print(string.find("ll"))     # 从左开始查找， 不存在返回-1
-print(string.rfind("ll"))     # 从右开始查找， 不存在返回-1
-print(string.index("ll"))     # 从左开始查找， 不存在抛异常
-print(string.rindex("ll"))     # 从右开始查找， 不存在抛异常
-print(string.endswith("llo"))     # 结束于
-print(string.startswith("hell"))     # 开始于
+from typing import List
+def minimum_length_encoding(self, words: List[str]) -> int:
+    sorted(words)
+    words.sort(key=lambda x: x[::-1])
+    return sum(len(v) + 1 for i, v in enumerate(words) if i == len(words) - 1 or not words[i + 1].endswith(v))
+
+def fun(words: List[str]) -> None:
+    words.sort()
+
+arr = [1, 9, 4, 7]
+arr.sort()
+print(arr)
