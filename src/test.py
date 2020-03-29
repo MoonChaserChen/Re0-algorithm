8a219a3951d2e80c3ead285404e07ca1d81b77e0
akira <chin.kou.akira@gmail.com>
2020-03-29 18:45:46 +0800

    change details about grammar

--- src/test.py
+++ src/test.py
@@ -1,6 +1,14 @@
-def minimum_length_encoding(words: [str]) -> int:
-    s = set(words)
-    for x in words:
-        for k in range(1, len(words)):
-            s.discard(x[k:])
-    return sum(len(x) + 1 for x in s)
+string = 'hello'
+print(string.isalnum())     # 所有字符都是数字或者字母
+print(string.isalpha())     # 所有字符都是字母
+print(string.isdigit())     # 所有字符都是数字
+print(string.islower())     # 所有字符都是小写
+print(string.isupper())     # 所有字符都是大写
+print(string.istitle())     # 所有单词都是首字母大写，像标题
+print(string.isspace())     # 所有字符都是空白字符、\t、\n、\r
+print(string.find("ll"))     # 从左开始查找， 不存在返回-1
+print(string.rfind("ll"))     # 从右开始查找， 不存在返回-1
+print(string.index("ll"))     # 从左开始查找， 不存在抛异常
+print(string.rindex("ll"))     # 从右开始查找， 不存在抛异常
+print(string.endswith("llo"))     # 结束于
+print(string.startswith("hell"))     # 开始于
