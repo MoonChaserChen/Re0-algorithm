9540067d9eec840d0a04b72541d8b56ec64282aa
akira <moon_chaser@163.com>
2020-03-31 00:26:47 +0800

    add article: 约瑟夫环.md

--- src/test.py
+++ src/test.py
@@ -1,19 +1,5 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-def remove_elements(head: ListNode, val: int) -> ListNode:
-    sentinel = ListNode(0)
-    sentinel.next = head
-
-    prev, curr = sentinel, head
-    while curr:
-        if curr.val == val:
-            prev.next = curr.next
-        else:
-            prev = curr
-        curr = curr.next
-
-    return sentinel.next
+def last_remaining(n: int, m: int) -> int:
+    re = 0
+    for i in range(2, n + 1):
+        re = (re + m) % i
+    return re
