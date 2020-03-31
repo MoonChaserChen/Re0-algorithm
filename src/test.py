c8f8b1096b4f6a6ed39c2afcdf2825ce40c4782d
akira <moon_chaser@163.com>
2020-03-31 20:47:42 +0800

    add articles about sort

--- src/test.py
+++ src/test.py
@@ -1,5 +1,24 @@
-def last_remaining(n: int, m: int) -> int:
-    re = 0
-    for i in range(2, n + 1):
-        re = (re + m) % i
-    return re
+from typing import List
+
+
+def merge_sort(nums, l, r):
+    if l == r:
+        return
+    mid = (l + r) // 2
+    merge_sort(nums, l, mid)
+    merge_sort(nums, mid + 1, r)
+    tmp = []
+    i, j = l, mid + 1
+    while i <= mid or j <= r:
+        if i > mid or (j <= r and nums[j] < nums[i]):
+            tmp.append(nums[j])
+            j += 1
+        else:
+            tmp.append(nums[i])
+            i += 1
+    nums[l: r + 1] = tmp
+
+
+def sort_array(nums: List[int]) -> List[int]:
+    merge_sort(nums, 0, len(nums) - 1)
+    return nums
