0a380cedc225baa47597aa772fb7455cb0f35f97
akira <moon_chaser@163.com>
2021-03-13 20:26:38 +0800

    add some article

--- src/test.py
+++ src/test.py
@@ -1,64 +1,23 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.pre = None
-        self.next = None
+def staff_table(table_arr, staff_arr):
+    def find_table(priority_arr):
+        for i in priority_arr:
+            for j in range(len(table_arr)):
+                if table_arr[j] == i:
+                    table_arr[j] += 1
+                    return j + 1
+    result = []
+    for x in staff_arr:
+        pa = [1, 0] if x == 'M' else [0, 1]
+        result.append(find_table(pa))
+    return result
 
 
-class LruCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.dic = {}
-        self.size = 0
-        self.dumb_head = Node(None, None)
-        self.dumb_tail = Node(None, None)
-        self.dumb_head.next = self.dumb_tail
-        self.dumb_tail.pre = self.dumb_head
-
-    @staticmethod
-    def delete_node(node):
-        node.pre.next = node.next
-        node.next.pre = node.pre
-
-    def add2head(self, node):
-        node.next = self.dumb_head.next
-        self.dumb_head.next.pre = node
-        self.dumb_head.next = node
-        node.pre = self.dumb_head
-
-    def set(self, key, value):
-        if key in self.dic:
-            node = self.dic[key]
-            node.value = value
-            self.delete_node(node)
-            self.add2head(node)
-        else:
-            if self.size == self.capacity:
-                self.dic.pop(self.dumb_tail.pre.key)
-                self.delete_node(self.dumb_tail.pre)
-            else:
-                self.size += 1
-            node = Node(key, value);
-            self.add2head(node)
-            self.dic[key] = node
-
-    def get(self, key):
-        if key not in self.dic:
-            return None
-        node = self.dic.get(key)
-        self.delete_node(node)
-        self.add2head(node)
-        return node.value
-
-
-cache = LruCache(2)
-cache.set(1, 10)
-cache.set(2, 20)
-print(cache.get(1))  # 10
-cache.set(3, 30)
-print(cache.get(2))  # None
-cache.set(4, 40)
-print(cache.get(1))  # None
-print(cache.get(3))  # 30
-print(cache.get(4))  # 40
+T = int(input())
+for i in range(T):
+    N = int(input())
+    ta = input()
+    M = int(input())
+    sa = input()
+    re = staff_table([int(x) for x in ta], [x for x in sa])
+    for x in re:
+        print(x)
