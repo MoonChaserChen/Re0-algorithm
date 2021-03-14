43be9fec3638702e1191349424660fb12d8e7d74
akira <moon_chaser@163.com>
2021-03-14 13:59:49 +0800

    add some article

--- src/test.py
+++ src/test.py
@@ -1,23 +1,54 @@
-def staff_table(table_arr, staff_arr):
-    def find_table(priority_arr):
-        for i in priority_arr:
-            for j in range(len(table_arr)):
-                if table_arr[j] == i:
-                    table_arr[j] += 1
-                    return j + 1
-    result = []
-    for x in staff_arr:
-        pa = [1, 0] if x == 'M' else [0, 1]
-        result.append(find_table(pa))
-    return result
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        return str(self.val)
+
+#
+#
+# @param head ListNode类
+# @param k int整型
+# @return ListNode类
+#
+class Solution:
+    def reverse_k_group(self, head, k):
+        if not head:
+            return head
+        h = t = head
+        for i in range(k):
+            if not t:
+                return head
+            t = t.next
+        new_head = reverse(h, t)
+        h.next = self.reverse_k_group(t, k)
+        return new_head
 
 
-T = int(input())
-for i in range(T):
-    N = int(input())
-    ta = input()
-    M = int(input())
-    sa = input()
-    re = staff_table([int(x) for x in ta], [x for x in sa])
-    for x in re:
-        print(x)
+# [h, t) 翻转
+def reverse(h, t):
+    p, c = None, h
+    while c != t:
+        temp = c.next
+        c.next = p
+        p = c
+        c = temp
+    return p
+
+
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n5 = ListNode(5)
+n6 = ListNode(6)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+# n5.next = n6
+
+
+r = Solution().reverse_k_group(n1, 3)
+print(r)
