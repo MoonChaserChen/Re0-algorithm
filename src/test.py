84208fd255d015a4c0637e9c5cdc14bb6b6e5606
akira <moon_chaser@163.com>
2021-03-21 12:48:07 +0800

    commit change

--- src/test.py
+++ src/test.py
@@ -1,11 +1,30 @@
-class ParkingSystem:
+def set_zeroes(matrix):
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    m, n = len(matrix), len(matrix[0])
+    # 首列是否原本存在0
+    flag_col0 = False
 
-    def __init__(self, big: int, medium: int, small: int):
-        self.count = small << 20 | medium << 10 | big
+    # 用matrix首行/列保存当前列/行是否存在0
+    for i in range(m):
+        if matrix[i][0] == 0:
+            flag_col0 = True
+        for j in range(1, n):
+            if matrix[i][j] == 0:
+                matrix[i][0] = matrix[0][j] = 0
 
-    def add_car(self, car_type: int) -> bool:
-        bit = (car_type - 1) * 10
-        if self.count & (0b1111111111 << bit) > 0:
-            self.count -= 1 << bit
-            return True
-        return False
+    for i in range(m - 1, -1, -1):
+        for j in range(1, n):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+        if flag_col0:
+            matrix[i][0] = 0
+
+
+mt = [[0, 1, 2, 0],
+      [3, 4, 5, 2],
+      [1, 3, 1, 5]]
+print(mt)
+set_zeroes(mt)
+print(mt)
