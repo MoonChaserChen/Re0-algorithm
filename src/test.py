4bc5dbbb0754dffdf5f0100e5ef799fe80b82fbf
akira <moon_chaser@163.com>
2021-03-22 11:20:47 +0800

    commit change

--- src/test.py
+++ src/test.py
@@ -1,30 +1,21 @@
-def set_zeroes(matrix):
-    """
-    Do not return anything, modify matrix in-place instead.
-    """
-    m, n = len(matrix), len(matrix[0])
-    # 首列是否原本存在0
-    flag_col0 = False
-
-    # 用matrix首行/列保存当前列/行是否存在0
-    for i in range(m):
-        if matrix[i][0] == 0:
-            flag_col0 = True
-        for j in range(1, n):
-            if matrix[i][j] == 0:
-                matrix[i][0] = matrix[0][j] = 0
-
-    for i in range(m - 1, -1, -1):
-        for j in range(1, n):
-            if matrix[i][0] == 0 or matrix[0][j] == 0:
-                matrix[i][j] = 0
-        if flag_col0:
-            matrix[i][0] = 0
+def hamming_weight(n: int) -> int:
+    n = n - ((n >> 1) & 0x55555555)
+    n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
+    n = (n + (n >> 4)) & 0x0f0f0f0f
+    n = n + (n >> 8)
+    n = n + (n >> 16)
+    return n & 0x3f
 
 
-mt = [[0, 1, 2, 0],
-      [3, 4, 5, 2],
-      [1, 3, 1, 5]]
-print(mt)
-set_zeroes(mt)
-print(mt)
+def hamming_weight_1(n: int) -> int:
+    n = (n & 0x55555555) + ((n >> 1) & 0x55555555)
+    n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
+    n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f)
+    n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff)
+    n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff)
+    return n
+
+
+print(hamming_weight(3), hamming_weight_1(3))
+print(hamming_weight(-3), hamming_weight_1(-3))
+print(hamming_weight(95), hamming_weight_1(95))
