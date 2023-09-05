845e0a87fac4c263c4e233ae30991409257ad7fa
Akira <chin.kou.akira@gmail.com>
2023-09-05 19:39:05 +0800

    移除依赖树中的多余部分

--- src/test.py
+++ src/test.py
@@ -1,2 +1,32 @@
-while True:
-    print(input())
+def parse(idx, s):
+    commands = []
+    command_open = False
+
+    command = ""
+    for x in s:
+        if x == '"':
+            command += '"'
+            command_open = not command_open
+        elif x == '_':
+            if command_open:
+                command += x
+            elif command != "":
+                commands.append(command)
+                command = ""
+        else:
+            command += x
+    if command != "":
+        commands.append(command)
+    le = len(commands)
+    if idx < 0 or idx > le - 1:
+        return "ERROR"
+    result = ""
+    for i, x in enumerate(commands):
+        if i == idx:
+            result += "_******"
+        else:
+            result += "_" + x
+    return result[1:]
+
+
+print(parse(int(input()), input()))
