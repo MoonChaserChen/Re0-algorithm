47207274ac1d247e2b002ed24c19e9218ed87c1f
Akira <chin.kou.akira@gmail.com>
2023-09-05 16:36:12 +0800

    移除依赖树中的多余部分

--- gen_sidebar.py
+++ gen_sidebar.py
@@ -1,15 +1,13 @@
 import os
 from shutil import copyfile
-import urllib.request
 import codecs
 import re
+from urllib.parse import quote
 
 suffix = ".md"
 gen_file_name = "summary.md"
 doc_path = "docs"
 read_me_file = "README.md"
-cname_file = "CNAME"
-ico_file = 'http://image.akira.ink/blog/favicon.ico'
 
 ignore_files = [gen_file_name, ".git", read_me_file, "images"]
 
@@ -32,15 +30,13 @@ def print_file(c_dir, depth, sidebar_file, readme_file):
         else:
             if suffix in f and f not in ignore_files:
                 f_n = os.path.splitext(f)[0]
-                content = "\t" * depth + "- [" + f_n + "](/" + re_f[2:] + ")\n"
+                content = "\t" * depth + "- [" + f_n + "](/" + quote(re_f[2:]) + ")\n"
                 sidebar_file.write(content)
                 readme_file.write(content)
 
 
 os.chdir(doc_path)
 copyfile("../" + read_me_file, read_me_file)
-copyfile("../" + cname_file, cname_file)
-urllib.request.urlretrieve(ico_file, 'favicon.ico')
 
 # 为README.md生成目录；生成左侧sidebar(summary.md)
 g_f = codecs.open(gen_file_name, 'w', encoding='utf-8')
