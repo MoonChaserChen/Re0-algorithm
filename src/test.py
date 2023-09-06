79be34e7c6cb639d90d8f7772bda0916bd008d34
Akira <chin.kou.akira@gmail.com>
2023-09-06 22:40:28 +0800

    no commit message

--- src/test.py
+++ src/test.py
@@ -1,38 +1,15 @@
-def split_str(strs: str, n: int):
-    str_list = strs.split("-")
-    split_list = []
-    for i in range(len(str_list)):
-        if i == 0:
-            split_list.append(str_list[i])
-        else:
-            j, k = 0, 0
-            curr_str = ""
-            while j < len(str_list[i]):
-                if k < n:
-                    curr_str += str_list[i][j]
-                    k += 1
-                else:
-                    split_list.append(curr_str)
-                    curr_str = str_list[i][j]
-                    k = 0
-                j += 1
-            if curr_str != "":
-                split_list.append(curr_str)
-    return "-".join(map(uni_case, split_list))
+def check_sub_str(strs: str, check: str):
+    i, j = 0, 0
+    while j < len(strs):
+        if strs[j] == check[i]:
+            i += 1
+            if i == len(check):
+                return True
+        j += 1
+    return False
 
 
-def uni_case(strs: str):
-    low_cnt, up_cnt = 0, 0
-    for x in strs:
-        if x.islower():
-            low_cnt += 1
-        elif x.isupper():
-            up_cnt += 1
-    if low_cnt > up_cnt:
-        return strs.lower()
-    elif up_cnt > low_cnt:
-        return strs.upper()
-    return strs
+print(check_sub_str("abcde", "bdskjfhla"))
+print(check_sub_str("abcde", "acf"))
 
 
-print(split_str("12abc-abCABc-4aB@", 3))
