3b76fa6856e3de86df47191162e4a690d2c43576
Akira <chin.kou.akira@gmail.com>
2023-09-12 17:52:04 +0800

    no commit message

--- src/test.py
+++ src/test.py
@@ -1,15 +1,18 @@
-def get_shortest_step(nums):
-    le = len(nums)
-    steps = []
-    for i in range(1, le // 2):
-        step = 1
-        while i < le - 1:
-            i += nums[i]
-            step += 1
-        if i == le - 1:
-            steps.append(step)
-    return -1 if len(steps) == 0 else min(steps)
+class Solution:
+    def __init__(self, _fixed_len, _arr):
+        self.fixed_len = _fixed_len
+        self.arr = _arr
 
+    @staticmethod
+    def init_from_console():
+        fixed_len = int(input())
+        return Solution([list(map(int, input().split(","))) for i in range(int(input()))])
 
-print(get_shortest_step([7, 5, 9, 4, 2, 6, 8, 3, 5, 4, 3, 9]))
-print(get_shortest_step([1, 2, 3, 7, 1, 5, 9, 3, 2, 1]))
+    def solve(self):
+        n = 0
+        for i in range(len(self.arr)):
+            for j in range(3):
+                self.arr[i][j] += min([x for k, x in enumerate(self.arr[i - 1]) if k != j])
+        return min(self.arr[-1])
+
+print([1, 2][3: 4])
