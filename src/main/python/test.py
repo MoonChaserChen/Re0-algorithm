eb68f143e3b4c03c618c298acc97e0d73faaf9ff
Akira <chin.kou.akira@gmail.com>
2023-09-25 15:54:38 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,14 +1,12 @@
-def merge_arr(num_arr):
-    result = []
-    num_arr.sort(key=lambda y: y[0])
-    for x in num_arr:
-        if not result or result[-1][1] < x[0]:
-            result.append(x)
-        else:
-            result[-1][1] = max(result[-1][1], x[1])
-    return result
+def rotate(matrix):
+    n = len(matrix)
+    for i in range((n + 1) // 2):
+        for j in range(n // 2):
+            matrix[i][j], matrix[n -1 - j][i], matrix[n - 1 - i][n - 1 - j], matrix[j][n - 1 - i] = \
+                matrix[n -1 - j][i], matrix[n - 1 - i][n - 1 - j], matrix[j][n - 1 - i], matrix[i][j]
+    return matrix
 
 
-assert merge_arr([[1, 3], [2, 6], [8, 10], [15, 18]]) == [[1, 6], [8, 10], [15, 18]]
-assert merge_arr([[1, 4], [4, 5]]) == [[1, 5]]
-assert merge_arr([[1, 4], [2, 3]]) == [[1, 4]]
+assert rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+assert rotate([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]) == [[15, 13, 2, 5], [14, 3, 4, 1],
+                                                                                   [12, 6, 8, 9], [16, 7, 10, 11]]
