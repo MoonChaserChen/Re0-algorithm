1e39cf4ec048911bc443f6f917a4f69823a2444b
Akira <chin.kou.akira@gmail.com>
2023-10-02 13:50:25 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,12 +1,31 @@
-def next_greater_element(nums1, nums2):
-    stack = []
-    m = {}
-    for i in range(len(nums2)):
-        while stack and nums2[stack[-1]] < nums2[i]:
-            m[nums2[stack.pop()]] = nums2[i]
-        stack.append(i)
-    return [m.get(x, -1) for x in nums1]
+def next_permutation(nums):
+    n = len(nums)
+    # 倒序找到第一个降序的值下标s_i
+    s_i = n - 2
+    while s_i >= 0 and nums[s_i] >= nums[s_i + 1]:
+        s_i -= 1
+    # 倒序找到第一个比nums[s_i]大的数并与nums[s_i]交换
+    if s_i >= 0:
+        b_i = n - 1
+        while nums[b_i] <= nums[s_i]:
+            b_i -= 1
+        nums[b_i], nums[s_i] = nums[s_i], nums[b_i]
+    # 将s_i后面的数逆序排列
+    lo, hi = s_i + 1, n - 1
+    while lo < hi:
+        nums[lo], nums[hi] = nums[hi], nums[lo]
+        hi -= 1
+        lo += 1
 
 
-assert next_greater_element([4, 1, 2], [1, 3, 4, 2]) == [-1, 3, -1]
-assert next_greater_element([2, 4], [1, 2, 3, 4]) == [3, -1]
+arr = [1, 2, 3]
+next_permutation(arr)
+assert arr == [1, 3, 2]
+
+arr = [1, 1, 5]
+next_permutation(arr)
+assert arr == [1, 5, 1]
+
+arr = [3, 2, 1]
+next_permutation(arr)
+assert arr == [1, 2, 3]
