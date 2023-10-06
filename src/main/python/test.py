d2b45da7b66a0eba40c7853acb0428356b1b37c9
Akira <chin.kou.akira@gmail.com>
2023-10-06 12:41:36 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,31 +1,23 @@
-def next_permutation(nums):
-    n = len(nums)
-    # 倒序找到第一个降序的值下标s_i
+def next_greater_element(num):
+    arr = list(str(num))
+    n = len(arr)
     s_i = n - 2
-    while s_i >= 0 and nums[s_i] >= nums[s_i + 1]:
+    while s_i >= 0 and arr[s_i] >= arr[s_i + 1]:
         s_i -= 1
-    # 倒序找到第一个比nums[s_i]大的数并与nums[s_i]交换
     if s_i >= 0:
         b_i = n - 1
-        while nums[b_i] <= nums[s_i]:
+        while arr[b_i] <= arr[s_i]:
             b_i -= 1
-        nums[b_i], nums[s_i] = nums[s_i], nums[b_i]
-    # 将s_i后面的数逆序排列
+        arr[s_i], arr[b_i] = arr[b_i], arr[s_i]
+    else:
+        return -1
     lo, hi = s_i + 1, n - 1
     while lo < hi:
-        nums[lo], nums[hi] = nums[hi], nums[lo]
-        hi -= 1
+        arr[lo], arr[hi] = arr[hi], arr[lo]
         lo += 1
+        hi -= 1
+    result = int("".join(arr))
+    return -1 if result > 2 ** 31 - 1 else result
 
-
-arr = [1, 2, 3]
-next_permutation(arr)
-assert arr == [1, 3, 2]
-
-arr = [1, 1, 5]
-next_permutation(arr)
-assert arr == [1, 5, 1]
-
-arr = [3, 2, 1]
-next_permutation(arr)
-assert arr == [1, 2, 3]
+assert next_greater_element(12) == 21
+assert next_greater_element(21) == -1
