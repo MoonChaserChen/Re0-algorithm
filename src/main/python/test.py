de1cf2e3f5a00b8d375a515fcaaf32546c19c5c1
Akira <chin.kou.akira@gmail.com>
2023-10-06 14:26:29 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,23 +1,10 @@
-def next_greater_element(num):
-    arr = list(str(num))
-    n = len(arr)
-    s_i = n - 2
-    while s_i >= 0 and arr[s_i] >= arr[s_i + 1]:
-        s_i -= 1
-    if s_i >= 0:
-        b_i = n - 1
-        while arr[b_i] <= arr[s_i]:
-            b_i -= 1
-        arr[s_i], arr[b_i] = arr[b_i], arr[s_i]
-    else:
-        return -1
-    lo, hi = s_i + 1, n - 1
-    while lo < hi:
-        arr[lo], arr[hi] = arr[hi], arr[lo]
-        lo += 1
-        hi -= 1
-    result = int("".join(arr))
-    return -1 if result > 2 ** 31 - 1 else result
+def max_profit(prices, fee):
+    f0, f1 = 0, -prices[0]
+    for x in prices:
+        f0 = max(f0, f1 + x - fee)
+        f1 = max(f1, f0 - x)
+    return f0
 
-assert next_greater_element(12) == 21
-assert next_greater_element(21) == -1
+
+assert max_profit([1, 3, 2, 8, 4, 9], 2) == 8
+assert max_profit([1, 3, 7, 5, 10, 3], 3) == 6
