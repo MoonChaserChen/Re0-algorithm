2ad74535c566d9f8101f8cee18de7d92e59e64ef
Akira <chin.kou.akira@gmail.com>
2023-10-07 22:16:27 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,10 +1,12 @@
-def max_profit(prices, fee):
-    f0, f1 = 0, -prices[0]
+def max_profit(prices):
+    f0, f1, f2 = -prices[0], 0, 0
     for x in prices:
-        f0 = max(f0, f1 + x - fee)
-        f1 = max(f1, f0 - x)
-    return f0
+        n_f0 = max(f0, f2 - x)
+        n_f1 = f0 + x
+        n_f2 = max(f1, f2)
+        f0, f1, f2 = n_f0, n_f1, n_f2
+    return max(f1, f2)
 
 
-assert max_profit([1, 3, 2, 8, 4, 9], 2) == 8
-assert max_profit([1, 3, 7, 5, 10, 3], 3) == 6
+assert max_profit([1, 2, 3, 0, 2]) == 3
+assert max_profit([1]) == 0
