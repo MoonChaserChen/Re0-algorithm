0fa2c46cb8dfb7b3190836e90b42d49819828fc2
Akira <chin.kou.akira@gmail.com>
2023-10-08 11:05:07 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,14 +1,39 @@
-def final_prices(prices):
-    stack = []
-    for i, price in enumerate(prices):
-        while stack and price <= prices[stack[-1]]:
-            t = stack.pop()
-            # 直接使用原prices变量保存结果
-            prices[t] = prices[t] - price
-        stack.append(i)
-    return prices
+from sortedcontainers import SortedList
 
 
-assert final_prices([8, 4, 6, 2, 3]) == [4, 2, 4, 2, 3]
-assert final_prices([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
-assert final_prices([10, 1, 1, 6]) == [9, 0, 1, 6]
+class StockPrice:
+    def __init__(self):
+        # minimum, maximum使用有序队列
+        # current使用Hash
+        self.sorted_prices = SortedList()
+        self.prices_map = {}
+        self.current_ts = 0
+
+    def update(self, timestamp: int, price: int) -> None:
+        if timestamp in self.prices_map:
+            self.sorted_prices.remove(self.prices_map[timestamp])
+        self.sorted_prices.add(price)
+        self.prices_map[timestamp] = price
+        self.current_ts = max(self.current_ts, timestamp)
+
+    def current(self) -> int:
+        return self.prices_map[self.current_ts]
+
+    def maximum(self) -> int:
+        return self.sorted_prices[-1]
+
+    def minimum(self) -> int:
+        return self.sorted_prices[0]
+
+
+stock_price = StockPrice()
+stock_price.update(1, 10)
+stock_price.update(2, 5)
+assert stock_price.current() == 5
+assert stock_price.maximum() == 10
+assert stock_price.minimum() == 5
+stock_price.update(1, 3)
+assert stock_price.maximum() == 5
+stock_price.update(4, 2)
+assert stock_price.minimum() == 2
+
