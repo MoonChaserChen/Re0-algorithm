af4e9eab081bfd770583f7b536135c93f8994872
Akira <chin.kou.akira@gmail.com>
2023-10-08 09:46:03 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,34 +1,14 @@
-import math
+def final_prices(prices):
+    stack = []
+    for i, price in enumerate(prices):
+        while stack and price <= prices[stack[-1]]:
+            t = stack.pop()
+            # 直接使用原prices变量保存结果
+            prices[t] = prices[t] - price
+        stack.append(i)
+    return prices
 
 
-class StockSpanner:
-    def __init__(self):
-        # 元素数量
-        self.cnt = 0
-        # 单调递减栈，栈中保存(元素值，下标)
-        self.stack = [(math.inf, -1)]
-
-    def next(self, val):
-        while self.stack[-1][0] < val:
-            self.stack.pop()
-        result = self.cnt - self.stack[-1][1]
-        self.stack.append((val, self.cnt))
-        self.cnt += 1
-        return result
-
-
-s1 = StockSpanner()
-assert s1.next(100) == 1
-assert s1.next(80) == 1
-assert s1.next(60) == 1
-assert s1.next(70) == 2
-assert s1.next(60) == 1
-assert s1.next(75) == 4
-assert s1.next(85) == 6
-
-s2 = StockSpanner()
-assert s2.next(31) == 1
-assert s2.next(41) == 2
-assert s2.next(48) == 3
-assert s2.next(59) == 4
-assert s2.next(79) == 5
+assert final_prices([8, 4, 6, 2, 3]) == [4, 2, 4, 2, 3]
+assert final_prices([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+assert final_prices([10, 1, 1, 6]) == [9, 0, 1, 6]
