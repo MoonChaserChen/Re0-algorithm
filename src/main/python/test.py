a27121106f5f387e99c674bcb685e38b8d846497
Akira <chin.kou.akira@gmail.com>
2023-10-09 12:10:24 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,39 +1,7 @@
-from sortedcontainers import SortedList
+def split_num(num: int) -> int:
+    num_str = "".join(sorted(str(num)))
+    num1, num2 = int(num_str[::2]), int(num_str[1::2])
+    return num1 + num2
 
-
-class StockPrice:
-    def __init__(self):
-        # minimum, maximum使用有序队列
-        # current使用Hash
-        self.sorted_prices = SortedList()
-        self.prices_map = {}
-        self.current_ts = 0
-
-    def update(self, timestamp: int, price: int) -> None:
-        if timestamp in self.prices_map:
-            self.sorted_prices.remove(self.prices_map[timestamp])
-        self.sorted_prices.add(price)
-        self.prices_map[timestamp] = price
-        self.current_ts = max(self.current_ts, timestamp)
-
-    def current(self) -> int:
-        return self.prices_map[self.current_ts]
-
-    def maximum(self) -> int:
-        return self.sorted_prices[-1]
-
-    def minimum(self) -> int:
-        return self.sorted_prices[0]
-
-
-stock_price = StockPrice()
-stock_price.update(1, 10)
-stock_price.update(2, 5)
-assert stock_price.current() == 5
-assert stock_price.maximum() == 10
-assert stock_price.minimum() == 5
-stock_price.update(1, 3)
-assert stock_price.maximum() == 5
-stock_price.update(4, 2)
-assert stock_price.minimum() == 2
-
+assert split_num(4325) == 59
+assert split_num(687) == 75
