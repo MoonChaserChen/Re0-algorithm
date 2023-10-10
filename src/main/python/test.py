31341e3dcc988c5922a55aded26257942de0880b
Akira <chin.kou.akira@gmail.com>
2023-10-10 09:21:43 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,7 +1,12 @@
-def split_num(num: int) -> int:
-    num_str = "".join(sorted(str(num)))
-    num1, num2 = int(num_str[::2]), int(num_str[1::2])
-    return num1 + num2
+from typing import List
 
-assert split_num(4325) == 59
-assert split_num(687) == 75
+
+def sum_distance(nums: List[int], s: str, d: int) -> int:
+    n = len(nums)
+    pos = [nums[i] - d if s[i] == 'L' else nums[i] + d for i in range(n)]
+    pos.sort()
+    return sum([(pos[i] - pos[i - 1]) * i * (n - i) for i in range(1, n)]) % (10**9 + 7)
+
+
+assert sum_distance([-2, 0, 2], "RLL", 3) == 8
+assert sum_distance([1, 0], "RL", 2) == 5
