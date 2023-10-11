68863931dd286b1eaad43e5a009dcea738ff1bec
Akira <chin.kou.akira@gmail.com>
2023-10-11 09:32:46 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,12 +1,12 @@
-from typing import List
-
-
-def sum_distance(nums: List[int], s: str, d: int) -> int:
-    n = len(nums)
-    pos = [nums[i] - d if s[i] == 'L' else nums[i] + d for i in range(n)]
-    pos.sort()
-    return sum([(pos[i] - pos[i - 1]) * i * (n - i) for i in range(1, n)]) % (10**9 + 7)
-
-
-assert sum_distance([-2, 0, 2], "RLL", 3) == 8
-assert sum_distance([1, 0], "RL", 2) == 5
+def top_students(positive_feedback, negative_feedback, report, student_id, k):
+    words = {}
+    for w in positive_feedback:
+        words[w] = 3
+    for w in negative_feedback:
+        words[w] = -1
+    arr = []
+    for s, i in zip(report, student_id):
+        score = sum(words.get(w, 0) for w in s.split())
+        arr.append([-score, i])
+    arr.sort()
+    return [i for v, i in arr[:k]]
