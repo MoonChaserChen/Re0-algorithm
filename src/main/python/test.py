713965d667b00cfc63bebdceee1f8aa71ca21098
Akira <chin.kou.akira@gmail.com>
2023-10-19 22:27:04 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,34 +1,9 @@
-import heapq
-import math
+from collections import Counter
+from functools import reduce
 from typing import List
 
 
-class MaxHeap:
-    def __init__(self, data=None):
-        if data is None:
-            self.data = []
-        else:
-            self.data = [-x for x in data]
-            heapq.heapify(self.data)
-
-    def push(self, item):
-        heapq.heappush(self.data, -item)
-
-    def pop(self):
-        return -heapq.heappop(self.data)
-
-
-def max_kelements(nums: List[int], k: int) -> int:
-    q = [-x for x in nums]
-    heapq.heapify(q)
-
-    ans = 0
-    for _ in range(k):
-        x = heapq.heappop(q)
-        ans += -x
-        heapq.heappush(q, -((-x + 2) // 3))
-    return ans
-
-
-assert max_kelements([10, 10, 10, 10, 10], 5) == 50
-assert max_kelements([1, 10, 3, 3, 3], 3) == 17
+def tuple_same_product(nums: List[int]) -> int:
+    n = len(nums)
+    c = Counter([nums[i] * nums[j] for i in range(n) for j in range(i + 1, n)])
+    return reduce(lambda x, y: x + y, [v * (v - 1) * 4 for _, v in c.items()])
