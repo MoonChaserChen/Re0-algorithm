2916f31e6f409ba2e046a00a5b5eba8f4b06dcdd
Akira <chin.kou.akira@gmail.com>
2023-10-21 22:33:48 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,9 +1,26 @@
-from collections import Counter
-from functools import reduce
-from typing import List
+class UnionFind:
+    def __init__(self, n: int):
+        self.roots = [i for i in range(n)]
+        # root值为当前树的节点数；非root值为1(其实没有意义)
+        self.sizes = [1] * n
 
+    def find(self, x: int) -> int:
+        if self.roots[x] == x:
+            return x
+        else:
+            self.roots[x] = self.find(self.roots[x])
+            return self.roots[x]
 
-def tuple_same_product(nums: List[int]) -> int:
-    n = len(nums)
-    c = Counter([nums[i] * nums[j] for i in range(n) for j in range(i + 1, n)])
-    return reduce(lambda x, y: x + y, [v * (v - 1) * 4 for _, v in c.items()])
+    def union(self, x: int, y: int):
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx != ry:
+            if self.sizes[rx] > self.sizes[ry]:
+                self.roots[ry] = rx
+                self.sizes[rx] += self.sizes[ry]
+            else:
+                self.roots[rx] = ry
+                self.sizes[ry] += self.sizes[rx]
+
+    def getSize(self, x: int) -> int:
+        return self.sizes[x]
