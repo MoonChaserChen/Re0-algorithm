832604fc1238784e0dc94326ced9331574a19b34
Akira <chin.kou.akira@gmail.com>
2023-10-22 18:17:31 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -2,18 +2,15 @@ from typing import List
 
 
 class Solution:
-    def minimumSum(self, nums: List[int]) -> int:
-        n = len(nums)
-        # pre[i]表示左侧最小值
-        pre, suf = nums.copy(), nums.copy()
-        for i in range(1, n):
-            pre[i] = min(pre[i], pre[i - 1])
-        for i in reversed(range(n - 1)):
-            suf[i] = min(suf[i], suf[i + 1])
-        ans = -1
-        for i in range(1, n - 1):
-            if pre[i - 1] < nums[i] and suf[i + 1] < nums[i]:
-                pans = nums[i] + pre[i - 1] + suf[i + 1]
-                if ans == -1 or ans > pans:
-                    ans = pans
-        return ans
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort(reverse=True)
+        i, s, res = 0, 0, 0
+        for i in range(len(satisfaction)):
+            s += satisfaction[i]
+            if s < 0:
+                return res
+            res += s
+        return res
+
+
+print(Solution().maxSatisfaction([-1, -8, 0, 5, -7]))
