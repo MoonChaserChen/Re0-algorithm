d05b6b1e6f80d74aa2b7074de689e3858b425cb7
Akira <chin.kou.akira@gmail.com>
2023-10-24 15:14:57 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,16 +1,17 @@
-from typing import List
-
-
 class Solution:
-    def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        satisfaction.sort(reverse=True)
-        i, s, res = 0, 0, 0
-        for i in range(len(satisfaction)):
-            s += satisfaction[i]
-            if s < 0:
-                return res
-            res += s
-        return res
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        mod = 10**9 + 7
+        dp = [1] * target if k >= target else [1] * k + [0] * (target - k)
+        for i in range(1, n):
+            for j in range(target - 1, -1, -1):
+                if j >= i:
+                    dp[j] = sum(dp[max(0, j - k):j]) % mod
+                else:
+                    dp[j] = 0
+        return dp[-1]
 
-
-print(Solution().maxSatisfaction([-1, -8, 0, 5, -7]))
+s = Solution()
+assert s.numRollsToTarget(1, 6, 3) == 1
+assert s.numRollsToTarget(2, 6, 7) == 6
+assert s.numRollsToTarget(30, 30, 500) == 222616187
+assert s.numRollsToTarget(2, 12, 8) == 7
