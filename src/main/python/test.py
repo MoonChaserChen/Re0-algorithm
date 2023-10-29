cc3112c6698396c92b6bda5542b952d8730f183a
Akira <chin.kou.akira@gmail.com>
2023-10-29 20:42:42 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,17 +1,31 @@
+from typing import List
+
+
 class Solution:
-    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        mod = 10**9 + 7
-        dp = [1] * target if k >= target else [1] * k + [0] * (target - k)
-        for i in range(1, n):
-            for j in range(target - 1, -1, -1):
-                if j >= i:
-                    dp[j] = sum(dp[max(0, j - k):j]) % mod
-                else:
-                    dp[j] = 0
-        return dp[-1]
+    """
+    至少发表了 h 篇论文，并且每篇论文 至少 被引用 h 次
+    """
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        # counter[i] 表示被引用了i次的论文数
+        counter = [0] * (n + 1)
+        for x in citations:
+            if x > n:
+                counter[n] += 1
+            else:
+                counter[x] += 1
+        tot = 0
+        # 从大到小不断check
+        for i in range(n, -1, -1):
+            # tot 表示至少被引用了i次的论文数
+            tot += counter[i]
+            # check： 发表了 i 篇论文，并且每篇论文 至少 被引用 i 次
+            if tot >= i:
+                return i
+
 
 s = Solution()
-assert s.numRollsToTarget(1, 6, 3) == 1
-assert s.numRollsToTarget(2, 6, 7) == 6
-assert s.numRollsToTarget(30, 30, 500) == 222616187
-assert s.numRollsToTarget(2, 12, 8) == 7
+assert s.hIndex([3, 0, 6, 1, 5]) == 3
+assert s.hIndex([1, 3, 1]) == 1
+assert s.hIndex([0, 0, 0]) == 0
+assert s.hIndex([8, 7, 6, 5, 3]) == 4
