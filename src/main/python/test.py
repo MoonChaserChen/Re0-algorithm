8f71972b16f828f788ada2b6b1b3372a9ee503f4
Akira <chin.kou.akira@gmail.com>
2023-11-09 00:18:58 +0800

    no commit message

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,9 +1,15 @@
-from functools import reduce
-from itertools import product
-from typing import List
-
-
 class Solution:
-    def maxProduct(self, words: List[str]) -> int:
-        masks = [reduce(lambda a, b: a | (1 << (ord(b) - ord('a'))), word, 0) for word in words]
-        return max((len(x[1]) * len(y[1]) for x, y in product(zip(masks, words), repeat=2) if x[0] & y[0] == 0), default=0)
+    def findTheLongestBalancedSubstring(self, s: str) -> int:
+        cnt = [0, 0]
+        res = 0
+        for x in s:
+            if x == "1":
+                cnt[1] += 1
+                res = max(res, min(cnt) * 2)
+            elif cnt[1] > 0:
+                cnt = [1, 0]
+            else:
+                cnt[0] += 1
+        return res
+
+print(Solution().findTheLongestBalancedSubstring("01000111"))
